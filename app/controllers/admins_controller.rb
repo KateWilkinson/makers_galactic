13c7e73c8d7e22a818906e135874d252062357dd
Triffanys101 <l.hargrave@hotmail.co.uk>
2015-09-04 15:05:40 +0100

    added some admin logic to display

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,15 @@
 class AdminsController < ApplicationController
 
   def index
-
+    @total = 0
+    if Ticket.any?
+      @tickets = Ticket.all
+      @tickets.each do |ticket|
+        @total = @total + ticket.price
+      end
+      @total
+    else
+      @total
+    end
   end
 end
