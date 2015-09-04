175daaa2b2aea2ccbeb50ba57119da549ffcd1ae
Jonathan Ng <jonngkh@hotmail.com>
2015-09-04 15:14:58 +0100

    Added flight availability logic

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,20 +2,21 @@ class FlightsController < ApplicationController
 
   def index
     @flight = Flight.find(available_flight)
+    @new_ticket = @flight.Ticket.new
   end
 
   private
 
     def available_flight
-      Tickets.all.each do |ticket|
-
-
-
-      # a flight with an id that shows up < 8 times in tickets.all
-
+      flights = Flight.all
+      flights.each do |flight|
+        if flight.available_seats > 0
+          flight.id
+        else
+          nil
+        end
+      end
     end
-
-
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904133950) do
+ActiveRecord::Schema.define(version: 20150904135241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "flights", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "aisle_price"
+    t.integer  "available_seats"
   end
 
   create_table "tickets", force: :cascade do |t|
