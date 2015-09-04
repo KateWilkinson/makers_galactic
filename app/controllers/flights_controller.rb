33afd7d6e92f319804a29031c3377579f3624fbd
Jonathan Ng <jonngkh@hotmail.com>
2015-09-04 14:48:06 +0100

    adds tests for flight availability

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,22 @@
 class FlightsController < ApplicationController
 
   def index
-  
+    @flight = Flight.find(available_flight)
   end
+
+  private
+
+    def available_flight
+      Tickets.all.each do |ticket|
+
+
+
+      # a flight with an id that shows up < 8 times in tickets.all
+
+    end
+
+
 end
+
+
+
--- spec/features/flight_feature_spec.rb
+++ spec/features/flight_feature_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 feature 'Booking a flight' do
-  scenario 'landing on homepage' do
+  scenario 'landing on homepage a user can book a flight' do
     visit '/flights'
-    expect(page).to have_content 'Book a flight'
+    expect(page).to have_content 'Seats available. Book a flight'
+  end
+
+  scenario 'landing on homepage a user cannot book a flight' do
+    visit '/flights'
+    expect(page).to have_content 'No seats available.'
+  end
+
+  scenario 'user should be able to fill in name' do
+    visit '/flights'
+    fill_in 'Name', with: 'John'
+    click_button 'Book'
+    expect(page).to have_content 'Your flight\'s been booked'
+  end
+
+  xscenario 'user should see price of seats' do
+    visit '/flights'
+    expect(page).to have_content "Each seat costs #{flight.ticket.price}"
   end
 end
